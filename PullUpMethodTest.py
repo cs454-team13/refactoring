bd719aa8f4a5de40db4f28b3e80e81f062555674
yunhyeeun <dmsdbsgP0320@gmail.com>
2020-12-09 23:42:00 +0900

    Fix testfile & Add PullUpMethodTest file

---
+++ PullUpMethodTest.py
@@ -0,0 +1,4 @@
+from Refactoring import *
+
+pullupmethod = PullUpMethod("simpleClass.py", "Korea", "get_name")
+pullupmethod.apply()
--- simpleClass.py
+++ simpleClass.py
@@ -3,14 +3,14 @@ class Country:
     def __init__(self):
         self.population = 0
 
-    def get_name(self):
-        return self.name
-
 class Korea(Country):
 
     def __init__(self):
         super(Country, self).__init__()
         self.name = 'Korea'
+    
+    def get_name(self):
+        return self.name
 
 class China(Country):
     
