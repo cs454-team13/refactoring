c94c345a94fee56ff2c9b6ed45480893b4c9b2a0
yunhyeeun <dmsdbsgP0320@gmail.com>
2020-12-09 23:49:14 +0900

    Rename pullupmethodtest file

--- PullUpMethodTest.py
+++ PullUpMethodTest.py
@@ -1,4 +1,4 @@
 from Refactoring import *
 
-pullupmethod = PullUpMethod("simpleClass.py", "Korea", "get_name")
+pullupmethod = PullUpMethod("pullupmethodclass.py", "Korea", "get_name")
 pullupmethod.apply()
